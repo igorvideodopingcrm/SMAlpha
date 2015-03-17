a12fbda80e100747937c9649d3f54a3c0cecb203
FofW <fofw@hotmail.fr>
2015-03-17 11:08:19 +0100

    Commit de l'agent équipement + modif mineure de l'agent météo

--- PROJET INFO 6ème semestre/Projet_Système_MultiAgents/src/AgentMeteo.java
+++ PROJET INFO 6ème semestre/Projet_Système_MultiAgents/src/AgentMeteo.java
@@ -8,13 +8,15 @@ import jade.core.behaviours.TickerBehaviour;
 public class AgentMeteo  extends Agent{
 	protected void setup(){
 		ParallelBehaviour meteoparallele = new ParallelBehaviour(ParallelBehaviour.WHEN_ANY);
-			meteoparallele.addSubBehaviour(new TickerBehaviour(this,3000){
+			
+		meteoparallele.addSubBehaviour(new TickerBehaviour(this,3000){
 				protected void onTick() {
 					//Espace ou Senor Meteo va chercher la météo sur le net pour l'afficher
 					//Ou la distribuer à qui de droit. Le timer ci dessus, 3000, est en millième de seconde (actuellement 3 s)
 					//A modifier selon la fréquence de rafraichissement voulue.
 				}
 			});
+		
 			meteoparallele.addSubBehaviour(new CyclicBehaviour(this) {
 				public void action()
 				{
@@ -22,5 +24,8 @@ public class AgentMeteo  extends Agent{
 					// et l'envoi de message pour donner les prévision météo
 				}
 			});
+			
+			addBehaviour(meteoparallele);
+			// ajout du comportement décrit au dessus.
 	}
 }
