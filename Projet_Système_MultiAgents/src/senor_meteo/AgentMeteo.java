a0616ba9636ad94da44e87419fb5ee2c842eeff6
FofW <fofw@hotmail.fr>
2015-04-07 11:52:49 +0200

    modif de la doc et ajout des fichiers météos (demander les JAR pour les faire fonctionner)

--- Projet_Système_MultiAgents/src/senor_meteo/AgentMeteo.java
+++ Projet_Système_MultiAgents/src/senor_meteo/AgentMeteo.java
@@ -1,9 +1,18 @@
 package senor_meteo;
+import java.io.IOException;
+
+import org.json.JSONException;
+
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.ParallelBehaviour;
 import jade.core.behaviours.TickerBehaviour;
 import jade.lang.acl.ACLMessage;
+import outilsmeteo.OwmClient;
+import outilsmeteo.WeatherData;
+import outilsmeteo.WeatherData.Clouds;
+import outilsmeteo.WeatherData.WeatherCondition;
+import outilsmeteo.WeatherStatusResponse;
 
 
 public class AgentMeteo  extends Agent{
@@ -12,6 +21,27 @@ public class AgentMeteo  extends Agent{
 			
 		meteoparallele.addSubBehaviour(new TickerBehaviour(this,3000){
 				protected void onTick() {
+					
+					
+					OwmClient owm = new OwmClient ();
+					WeatherStatusResponse currentWeather = null;
+					try {
+						currentWeather = owm.currentWeatherAtCity("Lapua","FI");
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					if (currentWeather.hasWeatherStatus ()) {
+					    WeatherData weather = currentWeather.getWeatherStatus ().get (0);
+				        float weatherCondition = weather.getTemp()-273;
+				        String description = String.valueOf(weatherCondition);
+					    System.out.println ("la température à saint pierre est de "+description);
+					}
+					
+					
 					//TODO Espace ou Senor Meteo va chercher la météo sur le net pour les agent en ayant besoin
 					//Ou la distribuer à qui de droit. Le timer ci dessus, 3000, est en millième de seconde (actuellement 3 s)
 					//A modifier selon la fréquence de rafraichissement voulue.
