ad88df827b90b27694cb25b339078f405e3131ac
aureliensenger <aureliensenger@hotmail.fr>
2015-05-12 19:49:08 +0200

    working on web interface

--- SMAlpha_html/meteo.php
+++ SMAlpha_html/meteo.php
@@ -3,12 +3,15 @@ include_once "./init.php";
 
 $tpl = new Smarty;
 
-$meteo_file=fopen("./res/meteo","r");
+$meteo_file=fopen("./res/meteo.txt","r");
 $meteo = array();
-while($day = fgets($meteo_file)){
-	$meteo[]=explode(":",$day);
+while($days = fgets($meteo_file)){
+	$meteo=explode(";",$days,-1);
 }
-
+for($i=0;$i<count($meteo);$i++){
+	$meteo[$i]=explode(",",$meteo[$i]);
+	$meteo[$i][0]=strstr($meteo[$i][0]," ",true);
+} 
 $tpl->assign("page","meteo");
 $tpl->assign("meteo",$meteo);
 $tpl->display('./view/header.html');
