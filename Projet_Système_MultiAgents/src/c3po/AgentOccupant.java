4d3b32a05255a110277f0aaffae66b450f2e5b25
FofW <fofw@hotmail.fr>
2015-05-14 16:24:19 +0200

    communication c3PO - web

--- Projet_Système_MultiAgents/src/c3po/AgentOccupant.java
+++ Projet_Système_MultiAgents/src/c3po/AgentOccupant.java
@@ -1,11 +1,29 @@
 package c3po;
 import java.io.IOException;
 
+import org.apache.http.ParseException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import senor_meteo.Tabmeteo;
+
+import java.util.ArrayList;
+
+//import src.BasicNameValuePair;
+//import src.ClientProtocolException;
+//import src.DefaultHttpClient;
+//import src.EditText;
+//import src.HttpClient;
+//import src.HttpPost;
+//import src.HttpResponse;
+//import src.NameValuePair;
+import org.apache.*;
+import java.lang.Exception;
+import java.lang.String;
+import java.io.UnsupportedEncodingException;
+
+//import src.UrlEncodedFormEntity;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
@@ -32,7 +50,7 @@ public class AgentOccupant  extends Agent{
 			occuparallele.addSubBehaviour(new OneShotBehaviour(this){
 				
 				public void action(){
-					System.out.println(getLocalName()+" lancé");
+					System.out.println(this.getClass().toString()+" lancé");
 					
 					ACLMessage message = new ACLMessage(ACLMessage.INFORM);
 					message.addReceiver(new AID("senor_meteo", AID.ISLOCALNAME));
@@ -61,7 +79,33 @@ public class AgentOccupant  extends Agent{
 				ACLMessage rep = new ACLMessage(ACLMessage.INFORM);
 				if(msg!= null)  {
 					String contenu=msg.getContent();
-					System.out.println("je suis C3PO et j'ai reçu " + contenu);
+					//System.out.println("je suis C3PO et j'ai reçu " + contenu);
+			//		HttpClient httpclient = new DefaultHttpClient();
+			//        HttpPost httppost = new HttpPost((String) params[0]);//rajouter de quoi joindre le serveur
+			        
+			//        ArrayList<NameValuePair> queryParams = new ArrayList<NameValuePair>();
+			//        queryParams.add(new BasicNameValuePair("contenu", contenu));
+			        
+			        // create and launch the POST request
+			        try {
+		//				httppost.setEntity(new UrlEncodedFormEntity(queryParams));
+		//		        HttpResponse httpResponse;
+		//				httpResponse = httpclient.execute(httppost);
+		//				response = EntityUtils.toString(httpResponse.getEntity());
+						/*JSONObject jsonResponse = new JSONObject(jsonString);
+						response = jsonResponse.get("message").toString();*/
+		//			} catch (UnsupportedEncodingException e) {
+						//Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+		//			}  catch (ClientProtocolException e) {
+						//Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+		//			} catch (IOException e) {
+						//Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+					}  catch (ParseException e) {
+						
+						//Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+					} //catch (JSONException e) {
+						//Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+					//}
 					}
 				else{
 					block();
