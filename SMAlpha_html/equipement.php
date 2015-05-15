083f15dacd144fe212d5c56aff8298b47f68247d
aureliensenger <aureliensenger@hotmail.fr>
2015-05-16 00:14:09 +0200

    CRUD equipement ok mais pas encore de vérification

---
+++ SMAlpha_html/equipement.php
@@ -0,0 +1,18 @@
+<?php
+include_once "./init.php";
+if(isset($_POST["create"])){
+	create_equipement($_POST["nom"],$_POST["conso"],$_POST["conso"],$_POST["fin"],$_POST["duree"]);
+}else if(isset($_POST["update"])){
+	update_equipement($_POST["id"],$_POST["nom"],$_POST["conso"],$_POST["conso"],$_POST["fin"],$_POST["duree"]);
+}else if(isset($_POST["delete"])){
+	delete_equipement($_POST["id"]);
+}
+//a penser, une intérogation depuis le java pour recupérer la liste des equipements
+$tpl = new Smarty;
+$equipements=get_equipements();
+$tpl->assign("page","equipement");
+$tpl->assign("equipements",$equipements);
+$tpl->display('./view/header.html');
+$tpl->display('./view/equipement.html');
+$tpl->display('./view/footer.html');
+?>
--- SMAlpha_html/model/equipement.php
+++ SMAlpha_html/model/equipement.php
@@ -2,20 +2,26 @@
 function create_equipement($nom,$conso,$debut,$fin,$duree){
 	if($fin<$debut+$duree)
 		$fin=$debut+$duree;
-	$query="INSERT INTO `equipement`(`nom`, `conso`, `debut_min`, `fin_max`, `durée`) VALUES ('".$nom."','".$conso."','".$debut."','".$fin."','".$duree."')";
+	$query="INSERT INTO `equipement`(`nom`, `conso`, `debut_min`, `fin_max`, `duree`) VALUES ('".$nom."','".$conso."','".$debut."','".$fin."','".$duree."')";
+// var_dump($query);
 	query($query);
 }
 
-function get_equipement(){
-	
+function get_equipements(){
+	$query="SELECT * FROM `equipement`";
+	$data=query($query);
+	return $data;
 }
 
-function update_equipement(){
-	
+function update_equipement($id,$nom,$conso,$debut,$fin,$duree){
+	$query="UPDATE `equipement` SET `nom`='".$nom."',`conso`='".$conso."',`debut_min`='".$debut."',`fin_max`='".$fin."',`duree`='".$duree."' WHERE id=".$id;
+// var_dump($query);	
+	query($query);
 }
 
-function delete_equipement(){
-	
+function delete_equipement($id){
+	$query="DELETE FROM `equipement` WHERE id=".$id;
+	query($query);
 }
 
 ?>
