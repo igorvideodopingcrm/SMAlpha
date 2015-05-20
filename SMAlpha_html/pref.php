d2c0ac32cf3a80b7a0c3c151274cbecdc8851d4e
asgard <asgard.thor@hotmail.fr>
2015-05-20 12:14:42 +0200

    preférences sur le serveur

--- SMAlpha_html/pref.php
+++ SMAlpha_html/pref.php
@@ -2,10 +2,17 @@
 
 
 include_once "./init.php";
+$prefs=get_pref();
 
+if(isset($_POST["update"])){
+	foreach($prefs as $key=>$value){
+		$prefs[$key]=$_POST[$key];
+	}
+	update_pref($prefs);
+}
 $tpl = new Smarty;
 $tpl->assign("page","pref");
-
+$tpl->assign("prefs",$prefs);
 $tpl->display('./view/header.html');
 $tpl->display('./view/pref.html');
 $tpl->display('./view/footer.html');
